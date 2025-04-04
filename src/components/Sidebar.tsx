d4fdd4a72731103a95ddc1193090cf1bcacd5f36
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-04-04 19:29:51 +0800

    refactor: update apiClient import and improve logout flow
    - Moved apiClient import from ../util/apiClient to ../api/apiClient.
    - Updated logout mutation to remove access and refresh tokens immediately after API response.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,7 @@ import {
 } from "@tabler/icons-react";
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
-import apiClient from "../util/apiClient";
+import apiClient from "../api/apiClient";
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -19,19 +19,19 @@ const Sidebar = () => {
         refresh: localStorage.getItem("refresh"),
       });
 
+      localStorage.removeItem("access");
+      localStorage.removeItem("refresh");
+
       return res.data;
     },
     onSuccess: () => {
-      localStorage.removeItem("token");
-      localStorage.removeItem("refresh");
-
       notifications.show({
         title: "Authentication",
         message: "Logout success!",
         color: "green",
       });
 
-      navigate("/login");
+      navigate("/");
     },
     onError: (error) => {
       notifications.show({
@@ -47,13 +47,13 @@ const Sidebar = () => {
       <div className="sticky top-0 left-0 h-screen flex flex-col justify-between border-r border-gray-700 w-20 md:w-full">
         <Stack gap={4} mt="md">
           <NavLink
-            onClick={() => navigate("/")}
+            onClick={() => navigate("/home")}
             label={<span className="hidden md:block">Home</span>}
             leftSection={<IconHome className="w-6 h-6 mx-auto md:mx-0" />}
             className="flex flex-col items-center md:flex-row md:items-center"
           />
           <NavLink
-            onClick={() => navigate("/profile")}
+            onClick={() => navigate("/home/profile")}
             label={<span className="hidden md:block">Profile</span>}
             leftSection={<IconUser className="w-6 h-6 mx-auto md:mx-0" />}
             className="flex flex-col items-center md:flex-row md:items-center"
