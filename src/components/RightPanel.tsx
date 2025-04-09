9e0dafd124ff2f8949084a808654184b8c98ca1a
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-04-10 02:25:46 +0800

    feat: integrate suggested profiles API with React Query

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -1,34 +1,46 @@
 import { Avatar, Button, Card } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+import { getSuggestedProfilesApi } from "../api/profiles";
+import { IProfile } from "../types/Profile";
 
 const RightPanel = () => {
   const navigate = useNavigate();
+
+  const { data } = useQuery({
+    queryKey: ["suggested-profiles"],
+    queryFn: getSuggestedProfilesApi,
+  });
 
   return (
     <div className="hidden lg:block my-4 mx-2 sticky top-2">
       <Card className="rounded-md">
         <p className="font-bold">Who to follow</p>
         <div className="flex flex-col gap-4">
-          {/* item */}
-          <div className="flex items-center justify-between gap-4">
+          {data?.map((profile: IProfile) => (
             <div
-              className="flex gap-2 items-center cursor-pointer"
-              onClick={() => navigate(`/profile`)}
+              key={profile.id}
+              className="flex items-center justify-between gap-4"
             >
-              <Avatar size="md" />
-              <div className="flex flex-col">
-                <span className="font-semibold tracking-tight truncate w-28">
-                  John Doe
-                </span>
-                <span className="text-sm text-slate-500">@johndoe</span>
+              <div
+                className="flex gap-2 items-center cursor-pointer"
+                onClick={() => navigate(`/home/profile/${profile.id}/`)}
+              >
+                <Avatar size="md" />
+                <div className="flex flex-col">
+                  <span className="font-semibold tracking-tight truncate w-28">
+                    {profile.full_name}
+                  </span>
+                  <span className="text-sm text-slate-500">
+                    {profile.username}
+                  </span>
+                </div>
               </div>
-            </div>
-            <div>
               <Button radius="xl" variant="outline">
                 Follow
               </Button>
             </div>
-          </div>
+          ))}
         </div>
       </Card>
     </div>
