c69563dfe2e1d0d1a6d5ff911b879551f356042e
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-04-17 11:07:43 +0300

    feat: implement PostDetail page with comment creation, deletion, and liking
    - Fetched and displayed post and paginated comments
    - Enabled text comment creation with form validation and notifications
    - Added support for comment deletion with confirmation modal
    - Implemented like/unlike toggle for comments
    - Integrated React Query for caching and data refetching

---
+++ src/pages/PostDetail.tsx
@@ -0,0 +1,336 @@
+import { Link, useParams } from "react-router-dom";
+import {
+  fetchPostById,
+  fetchCommentsByPostId,
+  createComment,
+  deleteComment,
+  likeComment,
+} from "../api/post";
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { Comments } from "../types/common";
+import {
+  ActionIcon,
+  Avatar,
+  Button,
+  Card,
+  Center,
+  Divider,
+  Group,
+  Loader,
+  Menu,
+  MenuDropdown,
+  MenuItem,
+  MenuTarget,
+  Stack,
+  Text,
+  Textarea,
+  Title,
+} from "@mantine/core";
+import {
+  IconArrowLeft,
+  IconCaretDownFilled,
+  IconDotsVertical,
+  IconHeart,
+} from "@tabler/icons-react";
+import Post from "../components/Post";
+import { useState } from "react";
+import { format } from "timeago.js";
+import { useAuth } from "../hooks/useAuth";
+import { notifications } from "@mantine/notifications";
+import { modals } from "@mantine/modals";
+
+const PostDetail = () => {
+  const { postId } = useParams();
+  const { authenticatedUser } = useAuth();
+  const queryClient = useQueryClient();
+
+  const [comment, setComment] = useState("");
+
+  const { data: post, isLoading: postLoading } = useQuery({
+    queryKey: ["post", postId],
+    queryFn: () => fetchPostById(postId!),
+  });
+
+  const {
+    data,
+    isLoading: commentsLoading,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery<Comments>({
+    queryKey: ["comments", postId],
+    queryFn: ({ pageParam = 1 }) => {
+      return fetchCommentsByPostId(postId!, pageParam);
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      if (!lastPage.next) return undefined;
+      const url = new URL(lastPage.next);
+      const nextPage = url.searchParams.get("page");
+      return nextPage ? parseInt(nextPage) : undefined;
+    },
+    enabled: !!postId,
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["comment"],
+    mutationFn: createComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["post", postId] });
+      queryClient.invalidateQueries({ queryKey: ["comments", postId] });
+      queryClient.invalidateQueries({ queryKey: ["feed"] });
+
+      setComment("");
+
+      notifications.show({
+        title: "Success",
+        message: "Comment posted!",
+        color: "green",
+      });
+    },
+    onError: (error) => {
+      notifications.show({
+        title: "Error",
+        message: error.message,
+        color: "red",
+      });
+    },
+  });
+
+  const { mutate: deleteCommentFn } = useMutation({
+    mutationKey: ["comment"],
+    mutationFn: deleteComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["post", postId] });
+      queryClient.invalidateQueries({ queryKey: ["comments", postId] });
+      queryClient.invalidateQueries({ queryKey: ["feed"] });
+      notifications.show({
+        title: "Success",
+        message: "Comment deleted!",
+        color: "green",
+      });
+    },
+    onError: (error) => {
+      notifications.show({
+        title: "Error",
+        message: error.message,
+        color: "red",
+      });
+    },
+  });
+
+  const { mutate: like, isPending: isLiking } = useMutation({
+    mutationFn: likeComment,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["comments", postId] });
+
+      notifications.show({
+        title: "Success",
+        message: data.liked ? "Comment liked!" : "Comment unliked!",
+        color: "green",
+      });
+    },
+    onError: (error) => {
+      notifications.show({
+        title: "Error",
+        message: error.message,
+        color: "red",
+      });
+    },
+  });
+
+  const handleSubmit = () => {
+    if (!postId) return;
+
+    const formData = new FormData();
+    formData.append("content", comment);
+    formData.append("comment_type", "text");
+
+    mutate({ postId: post!.id, formData });
+  };
+
+  const handleDelete = (commentId: number) => {
+    modals.openConfirmModal({
+      title: "Delete your comment",
+      children: (
+        <Text>
+          Are you sure you want to delete this comment? This action is
+          irreversible.
+        </Text>
+      ),
+      labels: { confirm: "Delete", cancel: "Cancel" },
+      confirmProps: { color: "red" },
+      onConfirm: () => deleteCommentFn({ postId: post!.id, commentId }),
+    });
+  };
+
+  const handleLike = (commentId: number) => {
+    like({ postId: post!.id, commentId });
+  };
+
+  const comments = data?.pages.flatMap((page) => page.results) || [];
+
+  const fetchNext = () => {
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  return (
+    <>
+      {/* Loading state */}
+      {postLoading && commentsLoading && (
+        <Center h="100vh">
+          <Loader size="lg" type="dots" />
+        </Center>
+      )}
+
+      <Stack gap="md">
+        {/* Header */}
+        <Group>
+          <Link to="/home">
+            <IconArrowLeft />
+          </Link>
+          <Title size="xl">Feed</Title>
+        </Group>
+
+        {/* Fetched post */}
+        {post && <Post post={post} />}
+
+        {/* Comment Input */}
+        <Stack gap="md">
+          <Textarea
+            placeholder="Leave a comment"
+            autosize
+            size="md"
+            minRows={2}
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+          />
+          <Group justify="end">
+            <Button
+              radius="xl"
+              variant="outline"
+              disabled={!comment}
+              loading={isPending}
+              onClick={handleSubmit}
+            >
+              Post
+            </Button>
+          </Group>
+        </Stack>
+
+        {comments.length > 0 && <Divider mb="md" />}
+
+        {/* Fetched comments */}
+        {comments.map((comment) => (
+          <Card
+            radius="md"
+            withBorder
+            p="md"
+            variant="unstyled"
+            key={comment.id}
+          >
+            <Group align="start" gap="xs">
+              {/* Avatar */}
+              <Link to={`/home/profile/${comment.author.id}`}>
+                <Avatar radius="xl" />
+              </Link>
+
+              <div className="flex flex-col flex-1">
+                <Group align="start" gap="xs">
+                  <Text size="sm">{comment.author.full_name}</Text>
+                  <Text size="sm" c="dimmed">
+                    @{comment.author.username}
+                  </Text>
+                  {comment.author.id === authenticatedUser.id && (
+                    <Menu position="bottom-end">
+                      <MenuTarget>
+                        <ActionIcon variant="transparent" className="ml-auto">
+                          <IconDotsVertical size={16} />
+                        </ActionIcon>
+                      </MenuTarget>
+
+                      <MenuDropdown>
+                        <MenuItem
+                          color="red"
+                          onClick={() => handleDelete(comment.id)}
+                        >
+                          Delete
+                        </MenuItem>
+                      </MenuDropdown>
+                    </Menu>
+                  )}
+                </Group>
+                <Group gap="xs">
+                  <Text size="xs" c="dimmed">
+                    {format(comment.created_at)}
+                  </Text>
+                </Group>
+
+                {/* Comment Content */}
+                <Text mt="sm">{comment.content}</Text>
+
+                <Divider my="sm" />
+
+                {/* Like count and Like button*/}
+                <Group gap="sm">
+                  <Group gap="xs">
+                    <Text size="sm" c="dimmed">
+                      {comment.like_count}
+                    </Text>
+                    <ActionIcon
+                      variant="transparent"
+                      size="xs"
+                      onClick={() => handleLike(comment.id)}
+                      disabled={isLiking}
+                    >
+                      {comment.is_liked ? (
+                        <IconHeart size={16} color="red" fill="red" />
+                      ) : (
+                        <IconHeart size={16} color="gray" />
+                      )}
+                    </ActionIcon>
+                  </Group>
+                </Group>
+              </div>
+            </Group>
+          </Card>
+        ))}
+
+        {comments.length > 0 && hasNextPage && !isFetchingNextPage && (
+          <ActionIcon
+            w="100%"
+            variant="outline"
+            disabled={!hasNextPage}
+            loading={isFetchingNextPage}
+            onClick={fetchNext}
+          >
+            <IconCaretDownFilled />
+          </ActionIcon>
+        )}
+
+        {/* No comments yet */}
+        {!commentsLoading && comments.length === 0 && !isPending && (
+          <Text c="dimmed" ta="center">
+            No comments yet 🥲
+          </Text>
+        )}
+
+        {/* No more comments to load */}
+        {!hasNextPage && !isFetchingNextPage && comments.length > 0 && (
+          <Text ta="center" c="dimmed">
+            You're all caught up!
+          </Text>
+        )}
+      </Stack>
+    </>
+  );
+};
+
+export default PostDetail;
