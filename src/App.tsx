3a80392d931c7f44e69f7c771490d395ef6b00de
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-04-20 14:59:47 +0300

    feat: add ProtectedRoutes to protect home and related routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
+import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import PostDetail from "./pages/PostDetail";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import PostDetail from "./pages/PostDetail";
+import ProtectedRoutes from "./routes/ProtectedRoutes";
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -17,10 +18,12 @@ const App = () => {
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
-          <Route path="/home" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="profile/:userId" element={<Profile />} />
-            <Route path="post/:postId" element={<PostDetail />} />
+          <Route path="/home" element={<ProtectedRoutes />}>
+            <Route element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="profile/:userId" element={<Profile />} />
+              <Route path="post/:postId" element={<PostDetail />} />
+            </Route>
           </Route>
         </Routes>
       </Router>
