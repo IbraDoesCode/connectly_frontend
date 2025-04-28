5419e123fbdf8a0340044d4634257f31ba43c469
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-04-28 11:04:24 +0300

    feat(auth): integrate GoogleOAuthProvider for Google login support

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,32 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ProtectedRoutes from "./routes/ProtectedRoutes";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const App = () => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
 
-          <Route path="/home" element={<ProtectedRoutes />}>
-            <Route element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="profile/:userId" element={<Profile />} />
-              <Route path="post/:postId" element={<PostDetail />} />
+            <Route path="/home" element={<ProtectedRoutes />}>
+              <Route element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="profile/:userId" element={<Profile />} />
+                <Route path="post/:postId" element={<PostDetail />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+          </Routes>
+        </Router>
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   );
 };
 
