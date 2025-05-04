9d28b0c6fe60ce2553bac012c19981797ba73571
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-05-04 12:41:38 +0300

    Refactor App and main entry to centralize global providers
    - Centralized all global context providers (`GoogleOAuthProvider`, `QueryClientProvider`, `MantineProvider`, etc.) in `main.tsx` for better separation of concerns.
    - Simplified `App.tsx` to focus solely on routing logic.

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,23 @@ import Profile from "./pages/Profile";
 import PostDetail from "./pages/PostDetail";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ProtectedRoutes from "./routes/ProtectedRoutes";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const App = () => {
-  const queryClient = new QueryClient();
-
   return (
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-
-            <Route path="/home" element={<ProtectedRoutes />}>
-              <Route element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="profile/:userId" element={<Profile />} />
-                <Route path="post/:postId" element={<PostDetail />} />
-              </Route>
-            </Route>
-          </Routes>
-        </Router>
-      </QueryClientProvider>
-    </GoogleOAuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<ProtectedRoutes />}>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="profile/:userId" element={<Profile />} />
+            <Route path="post/:postId" element={<PostDetail />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,24 @@ import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import { Notifications } from "@mantine/notifications";
 import { ModalsProvider } from "@mantine/modals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const queryClient = new QueryClient();
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider forceColorScheme="dark">
-      <Notifications />
-      <ModalsProvider>
-        <App />
-      </ModalsProvider>
-    </MantineProvider>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider forceColorScheme="dark">
+          <Notifications />
+          <ModalsProvider>
+            <App />
+          </ModalsProvider>
+        </MantineProvider>
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
